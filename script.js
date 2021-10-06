9ab9221496b4fa62b59e211837409ba9b599d7ac
Sidney Paucar <sidneypaucar@sidneys-mbp-2.myfiosgateway.com>
2021-10-06 11:07:46 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+const DOMAIN = "https://comicvine.gamespot.com/api/";
+const API_KEY = "35b9fcfad8457fe14de0144d69c23c67219e8fc3";
+const BASE_URL = `${DOMAIN}?apikey=${API_KEY}&`;
+
+const superheroDesc = [
+  {
+    id: "Wonder Woman", description:"Princess Diana"
+  },
+  {
+    id: "Flash", description:"Fastest Man Alive"
+  },
+  {
+    id: "Superman", description:"Strongest man alive"
+  },
+  {
+    id: "Mr. Fantastic", description:"Genius Intellect"
+  },
+  {
+    id: "Harley Quinn", description:"Joker partner"
+  },
+  {
+    id: "Wolverine", description:"Logan"
+  },
+  {
+    id: "Spider-Man", description:"WEb Slinger"
+  },
+  {
+    id: "Doctor Strange", description:"Mystic Arts"
+  },
+  {
+    id: "Captain America", description:"Americas Pride"
+  },  {
+    id: "Batman", description:"Worlds Greatest Detective"
+  },
+  {
+    id: "Hal Jordan", description:"Green Lantern"
+  },
+
+
+
+
+
+
+
+
+]
